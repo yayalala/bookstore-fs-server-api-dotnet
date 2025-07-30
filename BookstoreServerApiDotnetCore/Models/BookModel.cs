cf566eb9ca41e0fc9a2709492cd336e162ad7ec5
Yayalala <yaronl591@gmail.com>
2025-07-30 19:58:13 +0300

    Initial attempt to adapt backend book model to frontend book model

--- BookstoreServerApiDotnetCore/Models/BookModel.cs
+++ BookstoreServerApiDotnetCore/Models/BookModel.cs
@@ -3,8 +3,11 @@
     public class BookModel
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public AuthorModel Author { get; set; }
+        public required string Title { get; set; }
+        public required AuthorModel Author { get; set; }
+        public string Details { get; set; }
+        public required decimal Price { get; set; }
+        public decimal DiscountPercentage { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
